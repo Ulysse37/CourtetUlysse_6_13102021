3f9678e3aff070577728149c8c84baf196f6eeb7
Ulysse37 <ulysse.courtet@hotmail.fr>
2021-10-27 10:53:34 +0200

    Ajout fichier json + création fichier js

---
+++ js/jsonImport.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const data = require("../FishEyeData.json");
+console.log("data");
+
+/* fetch("../FishEyeData.json")
+  .then(response => response.json())
+  .then(json => console.log(json)); */
+
