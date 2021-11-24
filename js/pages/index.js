b5d7795cdb976390c7a02753a5893733d16e6add
Ulysse37 <ulysse.courtet@hotmail.fr>
2021-11-24 18:50:46 +0100

    Ajout / test de fetch()

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,5 +1,21 @@
 async function getPhotographers() {
     // Penser à remplacer par les données récupérées dans le json
+    fetch('../../FishEyeData.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("HTTP error" + response.status);
+        }
+        return response.json();
+    })
+    .then(json => {
+        this.users = json;
+        console.log(this.users);
+    })
+    .catch(function () {
+        this.dataError = true;
+    }) 
+    
+
     const photographers = [
         {
             "name": "Ma data test",
