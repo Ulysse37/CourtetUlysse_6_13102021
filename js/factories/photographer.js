8c2dd436437ae0ac1b29169bda1a6d2fca64a76f
Ulysse37 <ulysse.courtet@hotmail.fr>
2022-01-17 12:12:51 +0100

    Début du css de l'index + création class js

--- js/factories/photographer.js
+++ js/factories/photographer.js
@@ -5,19 +5,25 @@ function photographerFactory(data) {
     const picture = `images/photographers/${portrait}`;  
 
     function getUserCardDOM() {
-        const article = document.createElement('article');
-        const pictureElt = document.createElement('img');
+        const article = document.createElement("article");
+        const figureElt = document.createElement("figure");
+        const pictureElt = document.createElement("img");
+        const figCaptionElt = document.createElement("figcaption");
         pictureElt.setAttribute("src", picture);
-        const nameElt = document.createElement('h2');
+        const nameElt = document.createElement("h2");
         nameElt.textContent = name;  
-        const cityElt = document.createElement('p');
+        const cityElt = document.createElement("p");
         cityElt.textContent = city + ", " + country;
-        const taglineElt = document.createElement( 'p' );
+        cityElt.className = "location";
+        const taglineElt = document.createElement( "p" );
         taglineElt.textContent = tagline;
-        const priceElt = document.createElement( 'p' );
+        const priceElt = document.createElement( "p" );
         priceElt.textContent = price + "€/jour";
-        article.appendChild(pictureElt);
-        article.appendChild(nameElt);
+        priceElt.className = "prix";
+        article.appendChild(figureElt);
+        figureElt.appendChild(pictureElt);
+        figureElt.appendChild(figCaptionElt);
+        figCaptionElt.appendChild(nameElt);
         article.appendChild(cityElt);
         article.appendChild(taglineElt);
         article.appendChild(priceElt);
