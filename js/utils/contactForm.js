19d75a217727be13484278e46cea6b77014320fe
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-09-02 10:11:38 +0200

    aria hidden sur page photographe quand modale open

--- js/utils/contactForm.js
+++ js/utils/contactForm.js
@@ -1,18 +1,30 @@
 "use strict";
+
+const modal                 = document.getElementById("contact_modal");
+const photographerHeader    = document.querySelector(".photograph-header");
+const imageGallery          = document.querySelector(".photograph-gallery");
+const orderBy               = document.getElementById("orderBy");
+
 /**
  *Affiche la modale quand on appuie sur le bouton contactez-moi
  */
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
+
 	modal.style.display = "block";
+    photographerHeader.setAttribute("aria-hidden", "true");
+    imageGallery.setAttribute("aria-hidden", "true");
+    orderBy.setAttribute("aria-hidden", "true");
 }
 
 /**
  *Ferme la modale quand on appuie sur la croix 
  */
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
+
     modal.style.display = "none";
+    photographerHeader.setAttribute("aria-hidden", "false");
+    imageGallery.setAttribute("aria-hidden", "false");
+    orderBy.setAttribute("aria-hidden", "false");
 }
 
 const first     = document.getElementById("firstName");
